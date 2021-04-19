768396c9628084b94511bea7a019e6987b3ae19c
menglan-liu <menglan.liu7@gmail.com>
2021-04-19 19:18:33 -0400

    added DemoCucumber

---
+++ DemoCucumber/src/main/java/LearnMaven0401/DemoCucumber/App.java
@@ -0,0 +1,13 @@
+package LearnMaven0401.DemoCucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
